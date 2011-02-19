83880a753aacb37b85fe69e44c042ba4f1953d3d
Jerome Etienne <jerome.etienne@gmail.com>
2011-02-19 20:46:20 +0100

    more on greasemonkey support. not yet runnable tho 	modified:   bin/djsd-node 	modified:   bin/node-greasemonkey.js

--- bin/node-greasemonkey.js
+++ bin/node-greasemonkey.js
@@ -1,4 +1,31 @@
-#!/usr/bin/env node
+// should i publish this as a npm module ?
+// why not
+// should be an full object
+// - .match url
+
+exports.readdir	= function(dirname){
+	var baseNames	= []
+	try {
+		baseNames	= require('fs').readdirSync(dirname);
+	}catch(e){}
+	baseNames	= baseNames.filter(function(value){
+		return value.match(/\.user\.js$/);
+	});
+	return baseNames;
+}
+
+exports.parsedir	= function(dirname){
+	var baseNames	= exports.readdir(dirname);
+	var results	= [];
+	baseNames.forEach(function(baseName){
+		var filename	= dirname+"/"+baseName;
+		results.push({
+			filename	: filename,
+			meta		: exports.parseFile(filename)
+		})
+	})
+	return results;
+}
 
 exports.parseFile	= function(filename){
 	var data	= require('fs').readFileSync(filename).toString();
@@ -11,14 +38,20 @@ exports.parseData	= function(data){
 	// FIXIT here the begining ==UserScript== and end ==/UserScript==
 	// - should be tested... but i failed at multi line regexp
 	var matches	= tmpStr.match(/^\/\/ @(\S+)\s+(.+)/gm)
-	var gmOpts	= {}
+	var gmMeta	= {}
 	matches.forEach(function(str){
 		// FIXIT here there is a bug. it doesnt take the whole value if it contains space
 		var matches	= str.match(/^\/\/ @(\S+)\s+(.+)$/)
 		var key		= matches[1];
 		var val		= matches[2];
-		if( typeof gmOpts[key] === "undefined" ) gmOpts[key]	= [];
-		gmOpts[key].push(val);
+		if( typeof gmMeta[key] === "undefined" ) gmMeta[key]	= [];
+		gmMeta[key].push(val);
 	})
-	return gmOpts;	
+	return gmMeta;	
+}
+
+/**
+ * @return {Boolean} true if the url matches, false otherwise
+*/
+exports.matchUrl	= function(gmMeta, url){
 }
