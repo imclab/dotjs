8d69ec84e2d23f7470c40f585b9e220a67cd94a6
Jerome Etienne <jerome.etienne@gmail.com>
2011-02-21 01:16:36 +0100

    working greasemonkey support 	modified:   bin/djsd-node 	modified:   bin/node-greasemonkey.js

--- bin/node-greasemonkey.js
+++ bin/node-greasemonkey.js
@@ -50,8 +50,42 @@ exports.parseData	= function(data){
 	return gmMeta;	
 }
 
+exports.matchRule	= function(rule, url){
+	// convert the rule into a js regexp
+	var pattern	= "^"+rule;
+	// - replace "." by "\."
+	pattern	= pattern.replace(/\./g, '\\.')
+	// - replace "*" by "\*"
+	pattern	= pattern.replace(/\*/g, '.*')
+	// return true if the url matches the pattern, false otherwise	
+	return url.match(pattern) !== null;
+}
+
 /**
  * @return {Boolean} true if the url matches, false otherwise
 */
-exports.matchUrl	= function(gmMeta, url){
+exports.matchUrl	= function(gmScript, url){
+	// if a exclude rule matches, return false
+	if( gmScript.meta.exclude ){
+		for(var i = 0; i < gmScript.meta.exclude.length; i++){
+			var rule	= gmScript.meta.exclude[i];
+			if( exports.matchRule(rule, url) ){
+				return false;
+			}
+		}
+	}
+	// if a include rule matches, return true
+	if( gmScript.meta.include ){
+		for(var i = 0; i < gmScript.meta.include.length; i++){
+			var rule	= gmScript.meta.include[i];
+			if( exports.matchRule(rule, url) ){
+				return true;
+			}
+		}
+	}else{
+		// if there are no include rule, return true
+		return true;
+	}
+	// if this point is reached, return false
+	return false;
 }
